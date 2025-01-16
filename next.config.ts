18a0563f6b80f5a76ec342d702794bb6818c4cde
talyssonoliver <talyssondasilvaoliveira@gmail.com>
2025-01-16 21:41:30 +0000

    chore: configure project settings and add initial components
    - Configured ESLint and TypeScript settings
    - Updated .gitignore file
    - Set up Next.js configuration
    - Added initial Home page with Heading component
    - Installed necessary dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+    pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  }
+  
+  module.exports = nextConfig
