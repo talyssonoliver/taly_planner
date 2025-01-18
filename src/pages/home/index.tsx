b24fcdd22e323a8294953684e912dd513dfc03c8
talyssonoliver <talyssondasilvaoliveira@gmail.com>
2025-01-18 21:55:48 +0000

    feat: added PrimaAdapter and fixed some bugs on NextAuth

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,14 +4,12 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
-
-
 export default function Home() {
   return (
     <Container>
       <Hero>
         <Heading as="h1" size="4xl">Uncomplicated scheduling</Heading>
-        <Text size="xl">Connect your calendar and let people book you when you're free!</Text>
+        <Text size="xl">Connect your calendar and let people book you when you&apos;re free!</Text>
         <ClaimUsernameForm />
       </Hero>
       <Preview>
